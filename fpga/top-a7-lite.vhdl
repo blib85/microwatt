1083458ba1ba69c7f7e19f007653a12360c4d619
Matt Reverzani <mrev@posteo.de>
2026-02-08 08:35:42 +0800

    fix flash offset for 128mbit chip

--- fpga/top-a7-lite.vhdl
+++ fpga/top-a7-lite.vhdl
@@ -20,7 +20,7 @@ entity toplevel is
 	    USE_LITEDRAM       : boolean  := false;
 	    NO_BRAM            : boolean  := false;
 	    DISABLE_FLATTEN_CORE : boolean := false;
-        SPI_FLASH_OFFSET   : integer := 10485760;
+        SPI_FLASH_OFFSET   : integer := 8388608;
         SPI_FLASH_DEF_CKDV : natural := 1;
         SPI_FLASH_DEF_QUAD : boolean := true;
         LOG_LENGTH         : natural := 2048;
